02548fe822c8947f6dd74be7ec1b99ff8cf141ae
LemoneMascarpone <117305072+LemoneMascarpone@users.noreply.github.com>
2022-12-28 18:01:42 +0100

    Header erstellt

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Navbar from './components/Navbar'; 
 
+import Header from './components/Header';
 
 
 
@@ -10,6 +11,7 @@ function App() {
     <>
 
       <Navbar />
+      <Header />
 
       </>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Header () {
+    return (
+
+<header>
+    <img src="https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2813&q=80" alt="Eine Hand mit einer Blume" />
+  <title>Nachhaltiger Leben</title>
+  <h1>Nachhaltiger Leben</h1>
+    <h2>dein Einstieg leicht gemacht!</h2>
+</header>
+
+
+
+    );
+}
+
+
+export default Header;
+
+
