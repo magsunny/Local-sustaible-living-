549699fb5b34a2c6b1c1e43f732ef0e8a6943386
LemoneMascarpone <117305072+LemoneMascarpone@users.noreply.github.com>
2023-01-04 19:01:48 +0100

    neuer header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,17 @@ function Header () {
     return (
 
 <header>
-    <img src="https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2813&q=80" alt="Eine Hand mit einer Blume" />
-  <title>Nachhaltiger Leben</title>
-  <h1>Nachhaltiger Leben</h1>
-    <h2>dein Einstieg leicht gemacht!</h2>
+
+<div className="container flex min-w-full h-96  justify-center bg-center align-middle bg-[url('https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2813&q=80')]">
+      
+      <div className='text-white color: rgb(255 255 255); text-6xl tracking-wide font-style: italic ;' > <div className='align-middle h-full; '><h1>Nachhaltiger Leben</h1> </div>
+
+       <div><h2>dein Einstieg leicht gemacht!</h2> </div></div>
+
+</div>
+
+    
+
 </header>
 
 
