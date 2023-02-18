37882881dc48c34b901cdf7c9e0ddaa64778c928
magsunny@web.de <magsunny@web.de>
2023-02-18 20:45:10 +0100

    newsletter post

--- src/components/footer/LoginNewsletter.js
+++ src/components/footer/LoginNewsletter.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 const LoginNewsletter= (props) => {
 
 // use state for input field content
-const [newsletter, setNewsletter] = useState('');
+const [newmail, setNewsletter] = useState('');
 
 
 // sends information to server on clicking submit button or enter
@@ -14,9 +14,9 @@ const handleSubmit = (e) => {
  // e.preventDefault(); // prevents refreshing whole page
   const configuration = { //data contains request body for backend
     method: 'post',
-    url: 'http://localhost:3000/login', // endpoint 
+    url: 'http://localhost:3000/newsletter', // endpoint 
     data: {
-     newsletter,
+     newmail,
     },
   }
 
@@ -31,14 +31,14 @@ return (
 
    <div className="relative flex justify-center items-center">
 
-<form action='/post' method="POST" onSubmit={handleSubmit}>
+<form action='/newsletter' method="POST" onSubmit={handleSubmit}>
           
           <input
             type="email"
             Name="Mailadresse"
             placeholder="Mailadresse"
-            value={newsletter}
-                  onChange={(e) => setNewsletter(e.target.value)}
+            value={newmail}
+            onChange={(e) => setNewsletter(e.target.value)}
             className="text-gray-800
            sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"
           />
