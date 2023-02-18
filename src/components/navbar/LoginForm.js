85f2ec798ae5c112783735bfaea97aa409f98a19
Maike Osterhoff <mai.osterhoff@gmail.com>
2023-02-18 14:39:18 +0100

    logout verändert

--- src/components/navbar/LoginForm.js
+++ src/components/navbar/LoginForm.js
@@ -40,11 +40,6 @@ const handleSubmit = (e) => {
     })
 }
 
-const logout = () => {
-  cookies.remove('TOKEN', { path: ''});
-  setLogin(false);
-}
-
 // set pop up active or innactive
 const [isOpenLoginForm, setOpenLoginForm] = useState(false);
 const toggleLoginForm = () => {
@@ -161,7 +156,7 @@ useEffect(() => {
               {/* close button */}
               <div className="text-sm text-gray-600 mb-4 mt-2">
                   <span 
-                    onClick={logout}
+                    onClick={toggleLoginForm}
                     className="flex items-center justify-center cursor-pointer hover:text-gray-900">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="flex items-center justify-center w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
